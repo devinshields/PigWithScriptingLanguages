1c7e2a1e6e53ea996ea3818d7b74f62e9803f484
Devin Shields <devin.shields@gmail.com>
2013-03-02 00:34:33 -0500

    about to add a reduce demo

--- run.sh
+++ run.sh
@@ -10,19 +10,25 @@ rm -rf $DATA
 mkdir  $DATA
 mkdir  $RESULTS
 
-
-# SIMULATE PROCESS: generate some randomized structured data
+# SIMULATE SAMPLE DATA: generate some randomized structured data
 ./generatesample.py > $SAMPLE
 
 
-# PRE-HADOOP: test R streaming & python streaming via bash pipes
+
+
+# demonstrate pure line-by-line mapping
+# MAP TEST, PRE-HADOOP: test R streaming & python streaming via bash pipes
 cat $SAMPLE | ./mapper.py
 cat $SAMPLE | ./mapper.R
 
-
-# LOCAL DEV:    use pig local streaming to feed data into R, include the pig .jar
+# MAP TEST, LOCAL DEV:    use pig local streaming to feed data into R, include the pig .jar
 java -Xmx4096m -cp ./jars/pig-0.10.0.jar org.apache.pig.Main -param SAMPLE=$SAMPLE -param RESULTS=$RESULTS -x local streaming.pig 
 
+# MAP TEST, CLUSTER DEV:
+#pig streaming.pig -param SAMPLE=$SAMPLE -param RESULTS=$RESULTS
 
-# CLUSTER DEV:
-#pig streaming.pig -param SAMPLE=$SAMPLE
+
+
+
+
+
