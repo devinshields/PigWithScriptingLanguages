719a9f26f1cbbc9bb73c2da38d0d414ac25d2be1
Devin Shields <devin.shields@gmail.com>
2013-03-04 22:51:48 -0500

    dataframe construction via json is working

--- run.sh
+++ run.sh
@@ -13,17 +13,24 @@ mkdir  $RESULTS
 # SIMULATE SAMPLE DATA: generate some randomized structured data
 ./ServerLogEntry.py > $SAMPLE
 
+
 # LOCAL DEV: map only
 #java -Xmx4096m -cp ./lib/jars/pig-0.10.0.jar:./lib/jars/jython-2.5.0.jar org.apache.pig.Main -x local ./simple_map.pig
 
+
 # LOCAL DEV: reduce using python UDFs to organize data
 #java -Xmx4096m -cp ./lib/jars/pig-0.10.0.jar:./lib/jars/jython-2.5.0.jar org.apache.pig.Main -x local ./pig.py
+
+
 
 # LOCAL DEV: pass pig groups as JSON into R streaming
 java -Xmx4096m -cp ./lib/jars/pig-0.10.0.jar org.apache.pig.Main -x local ./store_group_as_json.pig
 cat ./data/groups_as_json/* > data/groups.json
 java -Xmx4096m -cp ./lib/jars/pig-0.10.0.jar org.apache.pig.Main -x local ./stream_group_as_json.pig
 
+
+# LOCAL DEV: test an rjson reducer
+#cat ./data/groups_as_json/p* | ./rjson.R
 
 
 
